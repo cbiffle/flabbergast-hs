06a64c8b35bf092bd213ae454f0ce406038d244f
Cliff L. Biffle <code@cliffle.com>
2018-06-21 09:44:56 -0700

    Improve uniqBy performance using HashSet.
    No reason to artificially penalize the algorithms that use uniqBy...

--- src/Uniq.hs
+++ src/Uniq.hs
@@ -1,20 +1,21 @@
 module Uniq where
 
-import Data.Set as S
+import Data.Hashable (Hashable)
+import Data.HashSet as H
 
 -- | Lazily removes duplicates from a list, where duplicates are determined by
 -- comparing the projections by `index`.
 --
--- For a list of n unique elements and d duplicates, this is O((n+d) log n) in
--- time, and O(n) in space. Algorithms that are already guaranteed to produce a
--- unique list should skip this.
-uniqBy :: (Eq b, Ord b) => (a -> b) -> [a] -> [a]
-uniqBy index = go S.empty
+-- For a list of n unique elements and d duplicates, this is amortized O(n+d)
+-- in time, and O(n) in space. Algorithms that are already guaranteed to
+-- produce a unique list should skip this.
+uniqBy :: (Eq b, Hashable b) => (a -> b) -> [a] -> [a]
+uniqBy index = go H.empty
   where
     go _ [] = []
     go seen (x : xs) =
       let xi = index x
-      in if index x `S.member` seen
+      in if index x `H.member` seen
             then go seen xs
-            else x : go (xi `S.insert` seen) xs
+            else x : go (xi `H.insert` seen) xs
 
