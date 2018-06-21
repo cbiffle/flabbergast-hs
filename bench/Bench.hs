0d20df6db4d815398cff2c403095e29bff2c9f2a
Cliff L. Biffle <code@cliffle.com>
2018-06-21 11:10:15 -0700

    Split benchmark code.

---
+++ bench/Bench.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE BangPatterns #-}
+
+module Bench where
+
+import Criterion.Main
+import Criterion.Types (reportFile)
+import Control.DeepSeq (force, NFData)
+import Paths_boggle
+import Base
+
+import qualified DP.Filtered1PV as DP
+import qualified Traversal.FilteredTrie as Trie
+import qualified Traversal.FilteredPrefixSet as Set
+
+loadDictFile :: FilePath -> IO RawDictionary
+loadDictFile name = do
+  p <- getDataFileName name
+  force . lines <$> readFile p
+
+board4x4 :: RawBoard
+board4x4 = ["INVS","TENE","APUU","GNDO"]
+
+
+-- | Benchmark that applies the solver repeatedly to a given dictionary and
+-- board, cooking each only once.
+solveBench :: forall s.
+              (Solver s, NFData (CookedDict s), NFData (CookedBoard s))
+           => RawDictionary -> RawBoard -> Benchmark
+solveBench d b =
+  let preD :: CookedDict s = cookDict @s d
+      preB :: CookedBoard s = cookBoard @s b
+  in bench "solve-only" $ nf (uncurry (solve @s)) (preD, preB)
+
+-- | Benchmark that applies the solver repeatedly to a given dictionary and
+-- board, cooking each every time.
+cookBench :: forall s. (Solver s, NFData (CookedDict s), NFData (CookedBoard s))
+          => RawDictionary -> RawBoard -> Benchmark
+cookBench d b = bench "cook-and-solve" $ nf (uncurry (cookAndSolve @s)) (d, b)
+
+comboBench :: forall s.
+              (Solver s, NFData (CookedDict s), NFData (CookedBoard s))
+           => String -> RawDictionary -> RawBoard -> Benchmark
+comboBench name d b = bgroup name
+    [ solveBench @s d b
+    , cookBench @s d b
+    ]
--- bench/Main.hs
+++ bench/Main.hs
@@ -8,38 +8,22 @@ import Criterion.Types (reportFile)
 import Control.DeepSeq (force, NFData)
 import Paths_boggle
 import Base
+import Bench
 
 import qualified DP.Filtered1PV as DP
 import qualified Traversal.FilteredTrie as Trie
-import qualified Traversal.FilteredPrefixSet as Set
+import qualified Traversal.FilteredPrefixHAMT as Set
 
-loadFile name = do
-  p <- getDataFileName name
-  force . lines <$> readFile p
-
-boards = [{- ("2x2", ["MS","UO"])
-         , ("3x3", ["TEI","EAA","ZJY"])
-         ,-} ("4x4", ["INVS","TENE","APUU","GNDO"])
-         ]
-
-sbench :: forall s. (Solver s, NFData (CookedDict s), NFData (CookedBoard s))
-          => String -> RawDictionary -> RawBoard -> Benchmark
-sbench name d b =
-  let preD :: CookedDict s = cookDict @s d
-      preB :: CookedBoard s = cookBoard @s b
-  in bgroup name
-    [ bench "solve-only" $ nf (uncurry (solve @s)) (preD, preB)
-    , bench "cook-and-solve" $ nf (uncurry (cookAndSolve @s)) (d, b)
-    ]
+boards = [("4x4", board4x4)]
 
 benches =
-  [ sbench @Set.T "set"
-  , sbench @Trie.T "trie"
-  , sbench @DP.T "dp"
+  [ comboBench @Set.T "set"
+  , comboBench @Trie.T "trie"
+  , comboBench @DP.T "dp"
   ]
 
 main = do
-  !rawDict <- loadFile "bench/dict.txt"
+  !rawDict <- loadDictFile "bench/dict.txt"
   let config = defaultConfig { reportFile = Just "boggle-bench.html" }
   defaultMainWith config $ flip map boards $ \(bname, board) ->
     bgroup bname $ flip map benches $ \f -> f rawDict board
