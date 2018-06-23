d11df5aab662265b700357b9eb35c871f1ea7816
Cliff L. Biffle <code@cliffle.com>
2018-06-23 11:52:01 -0700

    Add a Weigh-based memory allocation test.

---
+++ weigh/Main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+import Weigh
+
+import Data.Typeable
+import Control.DeepSeq (force, NFData)
+import Bench
+import Base
+
+import qualified DP.TwoPass
+import qualified DP.OnePass
+import qualified DP.OnePassSinglePath
+import qualified DP.Filtered1PV
+import qualified Traversal.Trie
+import qualified Traversal.FilteredTrie
+import qualified Traversal.FilteredPrefixHAMT
+import qualified Traversal.FilteredPrefixSet
+
+sfunc :: forall s. (Solver s, Typeable s)
+      => RawDictionary -> Weigh ()
+sfunc d = func name
+               (\(cd, b) -> solve @s cd (cookBoard @s b))
+               (force (d, board4x4))
+  where name = tyConModule $ typeRepTyCon $ typeRep (Proxy @s)
+
+main = do
+  !d <- force <$> loadDictFile "bench/dict.txt"
+  mainWith $ do
+    sfunc @DP.TwoPass.T d
+    sfunc @DP.OnePass.T d
+    sfunc @DP.OnePassSinglePath.T d
+    sfunc @DP.Filtered1PV.T d
+    sfunc @Traversal.Trie.T d
+    sfunc @Traversal.FilteredTrie.T d
+    sfunc @Traversal.FilteredPrefixHAMT.T d
+    sfunc @Traversal.FilteredPrefixSet.T d
